3fe97281d17e1d58eaf6e0e355c7d79cf6f7afa8
shobeedev <moha.saoud22@gmail.com>
2026-03-04 10:02:09 +0000

    Done

--- CH01/01_12/01_12-challenge1.c
+++ CH01/01_12/01_12-challenge1.c
@@ -1,9 +1,9 @@
-main program
- 
-initialization
-
-initial message
-
-main program loop
-
-output results
+#include <stdio.h>
+#include <stdlib.h>
+//the first commment outside the main function.
+int main()
+{
+	//the other line are commment inside the main function.
+	printf("Done!\n");
+	return 0;
+}
